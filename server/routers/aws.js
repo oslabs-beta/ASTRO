76a310a03b543f98bfc5b1f54d597227391cfda2
Adam White <adamkarnwhite@gmail.com>
2022-03-05 11:25:10 -0500

    adds basic aws controller filestructure and dependencies

--- server/routers/aws.js
+++ server/routers/aws.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //AWS middleware
-const getFunctions = require('../controllers/aws/Metrics/getLambdaFuncs');
+const getFunctions = require('../controllers/aws/metrics/getLambdaFuncs');
 
 //Returning all Lambda funcs
 router.route('/getLambdaFunctions').post(getFunctions, (req, res) => {
