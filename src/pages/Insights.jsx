36794b334762deeae332b8031e7caae9dccfe3f1
Michelle Shahid <shahidmichelle@gmail.com>
2022-03-09 14:13:31 -0500

    moved sidebar to appropriate component

--- src/pages/Insights.jsx
+++ src/pages/Insights.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { SideBar } from '../components/Sidebar.jsx';
 
 function Insights(){
     return (
         <div>
+            {/* <SideBar/> */}
             <h1>Insights page</h1>
         </div>
     );
