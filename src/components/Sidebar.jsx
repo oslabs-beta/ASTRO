9dfa5ef92e1a68f1c973ce6cdeb9f9f9c019628c
Michelle Shahid <shahidmichelle@gmail.com>
2022-03-08 23:22:09 -0500

    Rendered list of functions from fetch

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,57 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { data } from '../utils/getLambdaFunctions.js'
 
-const sideBar = () =>{
-  const result = [];
+export const SideBar = () => {
 
+const [result, setResult] = useState([])
 
-fetch('http://localhost:1111/aws/getLambdaFunctions', {
-     method: "POST",
-     headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        region: "us-east-1",
-        credentials: {
-            accessKeyId: "AKIA5TZBMT7EJNT2IGMU",
-            secretAccessKey: "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"
-        }
-      })
- })
- .then((response) => response.json())
- .then(data => {
-  // const result = [];
-   for (let i = 0; i < data.length; i++) {
-     const el = data[i];
-    //  console.log('for loop', el)
-     result.push(el)
-   }
-   console.log(result.length)
- })
- .catch((err) => console.log(err))
+useEffect(() => {
+  const res = Promise.resolve(data()).then(res => setResult(res)).catch(err => console.log(err))
+},[])
 
- 
-//  .then((data) => console.log('data', data.json()))
-//  .catch((err) => console.log(err))
-// console.log('The response is right here', funcNames)
-// }
- //console.log('this is func list', funcList);
-//  funcNames();
-// console.log('this is result', result)
-// const buttons = [];
+/**
+ * @Remember { Axios }
+ */
 
-// for(let i = 0; i < result.length; i++){
-//   console.log('func', i)
-//     buttons.push(<div>{result[i]}</div>);
-// }
-
+console.log(result)
 return(
     <div>
-      {/* {buttons} */}
-       <h1>Sidebar</h1>
+        { ( result && result.length > 0 ) && result.map((res, idx) => <div key={idx}>{res}</div>) }
     </div>
     )
 
 }
-
-export default sideBar;
