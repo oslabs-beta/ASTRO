18de06ff6b0c54b0e2119605a4812c093a44cce7
samhcarrasco <samuelcarrasco@Samuels-Air.fios-router.home>
2022-03-10 16:59:04 -0500

    insightscomponent

--- src/pages/Insights.jsx
+++ src/pages/Insights.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { SideBar } from '../components/Sidebar.jsx';
+import Metrics from '../components/Metrics.jsx';
+import { metricsByFunc } from '../utils/getMetricsByFunc.js'
+import { useEffect, useState } from 'react'
+import { Link, Outlet } from 'react-router-dom';
 
 function Insights(){
+
     return (
         <div>
-            {/* <SideBar/> */}
-            <h1>Insights page</h1>
+            <SideBar/>
+            <h1>Lambda Func</h1>
+            <Link to="/insights/metrics/"> Invocations </Link>  |
+            <Link to="/insights/metrics/"> Errors </Link>  |
+            <Link to="/insights/metrics/"> Throttles </Link> 
+            
+             <Metrics/> 
+            <Outlet/>
         </div>
     );
 }
