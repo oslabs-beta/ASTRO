5e3fb9e6cb75c59d825fd753537e1d7359df581c
Anthony <adavid16967@gmail.com>
2022-03-21 15:48:11 -0400

    added test folder with backend and front end folders > > Co-authored-by: Michelle Shahid shahidmichelle@gmail.com

---
+++ __test__/frontend/Insights.test.js
@@ -0,0 +1,23 @@
+
+/*
+unit testing - testing a small part of code
+integration testing - multiple units working together
+end to end - testing from front to backend
+
+possible tests we can implement:
+
+-registration page does not let you submit until you fill all required inputs
+-login page does not let you login unless you have correct username/password
+-and onclick or onchange events including toggling the sidebar or navbar
+
+
+*/
+// import { FiberPinRounded } from "@mui/icons-material";
+import { render, screen } from "@testing-library/react"
+import Insights from "../../src/pages/Insights";
+
+test('on initial render, the insights page should render the dashboard and change on user click', () => {
+	render(< Insights />)
+
+	screen.debug();
+})
