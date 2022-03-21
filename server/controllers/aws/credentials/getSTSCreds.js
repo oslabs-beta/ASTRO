a8f8afed1b19e54e70833e8f28c13426f57d7933
nehreen <nehreenanamatgmaildotcom>
2022-03-21 15:50:57 -0400

    added temporary security credentials to improve auth flow
    Co-authored-by: Adam adamkarnwhite@gmail.com

--- server/controllers/aws/credentials/getSTSCreds.js
+++ server/controllers/aws/credentials/getSTSCreds.js
@@ -1,72 +1,74 @@
 const AWSUtilFunc = require('../Metrics/utils/AWSUtilFunc.js');
 const STSCreds = {};
-
-const { AssumeRoleCommand, STSClient } = require('@aws-sdk/client-sts');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const {
-  Lambda,
-  LambdaClient,
-  ListFunctionsCommand,
+	AssumeRoleCommand,
+	STSClient,
+} = require('@aws-sdk/client-sts');
+
+const {
+	Lambda,
+	LambdaClient,
+	ListFunctionsCommand,
 } = require('@aws-sdk/client-lambda');
 
 const {
-  CloudWatchClient,
-  GetMetricDataCommand,
+	CloudWatchClient,
+	GetMetricDataCommand,
 } = require('@aws-sdk/client-cloudwatch');
 
 STSCreds.get = async (req, res, next) => {
-  const roleParams = {
-    RoleArn: res.locals.arn,
-    RoleSessionName: 'AstroSession',
-  };
-}
-const stsClient = new STSClient();
+	const roleParams = {
+		RoleArn: res.locals.arn,
+		RoleSessionName: 'AstroSession',
+	};
 
-/*
-const dotenv = require('dotenv');
-// const { fromIni } = require('@aws-sdk/credential-provider-ini');
-const { STSClient } = require('@aws-sdk/client-sts');
+	const credentials = {
+		accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+		secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+	};
 
-dotenv.config();
+	const region = process.env.AWS_REGION;
 
-// root user credentials
-const credentials = {
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-};
-const region = process.env.AWS_REGION;
+	const stsClient = await new STSClient({
+		credentials,
+		region,
+	});
 
-// Create an Amazon CloudWatch Logs service client object.
-const stsClient = new STSClient({
-  region: region,
-  credentials: credentials,
-});
+	try {
+		// Class AssumeRole Command returns a temporary access key ID, secret access key, and security token to access AWS resources
+		// these temporary security credentials can be used to access any AWS resource
+		const assumedRole = await stsClient.send(
+			new AssumeRoleCommand(roleParams)
+		);
+		const accessKeyId = assumedRole.Credentials.AccessKeyId;
+		const secretAccessKey = assumedRole.Credentials.SecretAccessKey;
+		const sessionToken = assumedRole.Credentials.SessionToken;
 
-module.exports = {
-  stsClient,
-};
+    // what gets sent back to the client
+		res.locals.STSCreds = {
+			credentials: {
+				accessKeyId,
+				secretAccessKey,
+				sessionToken,
+			},
+			region,
+		};
 
-*/
-
-try {
-  // Class AssumeRole Command returns a temporary access key ID, secret access key, and security token to access AWS resources
-  // these temporary security credentials can be used to access any AWS resource
-  const assumedRole = await stsClient.send(new AssumeRoleCommand(roleParams));
-  const accessKeyId = assumedRole.Credentials.AccessKeyId;
-  const secretAccessKey = assumedRole.Credentials.SecretAccessKey;
-  const sessionToken = assumedRole.Credentials.SessionToken;
-
-  res.locals.STScreds = { accessKeyId, secretAccessKey, sessionToken };
-
-  console.log('temporary credentials from getSTSCreds.js are: ', roleParams)
-  return next();
-
-} catch (err) {
-  if (err) {
-    console.error(err);
-    return next(err);
-  }
+		// console.log(
+		// 	'temporary credentials from getSTSCreds.js are: ',
+		// 	res.locals.STScreds
+		// );
+		return next();
+    
+	} catch (err) {
+		if (err) {
+			console.error(err);
+			return next(err);
+		}
+	}
 };
 
 module.exports = STSCreds;
-
